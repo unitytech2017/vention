dbb462f4b575007dc37b392b982e5b7880b1f4f4
unitytech2017 <143252063+unitytech2017@users.noreply.github.com>
2025-12-14 17:30:17 +0900

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,18 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
-  // 기본 Vite 설정
+  build: {
+    rollupOptions: {
+      input: {
+        main: './index.html',
+        'idea-chatbot': './idea-chatbot.html',
+        'patent-chatbot': './patent-chatbot.html',
+        'ai-sketch': './ai-sketch.html',
+        '3d-modeling': './3d-modeling.html',
+        '3d-view': './3d-view.html',
+        'student': './student.html',
+        'teacherMonitor': './teacherMonitor.html',
+      }
+    }
+  }
 })
-
